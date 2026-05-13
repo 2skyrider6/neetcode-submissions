7e3898c12e5f96a2723979da3e42a4ae69d2eb7b
Prajyot Ambarwele <159532207+2skyrider6@users.noreply.github.com>
2026-05-13 16:22:59 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.py
@@ -0,0 +1,33 @@
+from typing import List
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        longest_streak = 0
+
+        for num in num_set:
+            # Check if this is the FIRST number of a sequence
+            # (i.e., the number before it is not in the set)
+            if (num - 1) not in num_set:
+                current_num = num
+                current_streak = 1
+
+                # Keep checking if the next consecutive numbers exist
+                while (current_num + 1) in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                # Update the maximum sequence length found so far
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
+  
+
+
+                    
+
+
+
+
+
+
+        
